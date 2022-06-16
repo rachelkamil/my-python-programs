bf06e70f1fc4157515b2835d809dc191936abd74
Rachel Kamil <rachel.ruoqi.kamil@gmail.com>
2022-06-16 20:48:35 +0000

    stuff

---
+++ python-projects/project-01-madlibs.py
@@ -0,0 +1,7 @@
+adj   = input("Adjective: ")
+verb1 = input("     Verb: ")
+noun  = input("     Noun: ")
+verb2 = input("     Verb: ")
+
+madlib = f"Computer programming is so {adj}! I love it because I can {verb1} on it. I have a pet {noun}, he can run and {verb2}!"
+print(madlib)
